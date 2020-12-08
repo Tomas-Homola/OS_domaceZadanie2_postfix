9c07720ae1adb8ae1f9d7f26382704af6d4634d9
Tomas-Homola <homola.tom6@gmail.com>
2020-12-08 15:06:33 +0100

    Zadanie done

--- postfix.py
+++ postfix.py
@@ -5,19 +5,23 @@ def eval_expr(s):
     temp = []
     s = s.split() # rozdelenie na list
 
-    for i in range(0, len(s)): # premena cisel na int
-        if (s[i].isnumeric()):
-            s[i] = int(s[i])
-
     for item in s:
         
-        if (item != "+"):
+        if (item.isnumeric()): # ak to je cislo, tak sa prida do temp zoznamu
             temp.append(item)
-        else:
-            a = temp.pop()
-            b = temp.pop()
-
-            temp.append(a + b)
+        else: # ak to nie je cislo, tak uz sa predpoklada, ze v temp zozname su uz 2 cisla, tie sa vyberu a daju do "a" a "b"
+            a = int(temp.pop())
+            b = int(temp.pop())
+            
+            # potom podla toho, ze ake je znamienko, sa vyhodnoti vysledok
+            if (item == "+"):
+                temp.append(a + b)
+            elif (item == "-"):
+                temp.append(b - a)
+            elif (item == "*"):
+                temp.append(a * b)
+            elif (item == "/"):
+                temp.append(b // a)
 
 
     return temp[0]
